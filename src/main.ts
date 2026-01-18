9fd857f8350daca1d31064f38f17d34c7ad7e08b
USS <franckmekoulou.dev@hotmail.com>
2026-01-18 15:39:53 +0000

    Initial Commit NestJS Default Application

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap()
+  .then(() => console.log('Listening on port 3000'))
+  .catch((err) => console.log(err));
